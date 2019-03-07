03a72ef4489379c35dca970e777bfa88b21f7eea
Henri <henriclocuh@yahoo.fr>
2019-03-07 19:50:21 +0100

    Création du champs fullname dans la table user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190303172302) do
+ActiveRecord::Schema.define(version: 20190307183857) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20190303172302) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "fullname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
